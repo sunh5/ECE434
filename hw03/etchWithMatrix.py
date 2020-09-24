c1c49c449e6ed01130f7a0fde5fb4def1c7d24ab
Ray Sun <sunh5@Rose-Hulman.edu>
2020-09-24 09:08:40 +0000

    update

---
+++ hw03/etchWithMatrix.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python3
+#   Written by Haoxuan Sun 9/16/2020
+#   Homework 02
+#   This code is to implement the function of Etch-a-sketch with gpio
+import numpy
+import Adafruit_BBIO.GPIO as GPIO
+import time
+import smbus
+import time
+import pdb; 
+
+bus = smbus.SMBus(2)  # Use i2c bus 1
+matrix = 0x70         # Use address 0x70
+delay = 1; # Delay between images in s
+
+bus.write_byte_data(matrix, 0x21, 0)   # Start oscillator (p10)
+bus.write_byte_data(matrix, 0x81, 0)   # Disp on, blink off (p11)
+bus.write_byte_data(matrix, 0xe7, 0)   # Full brightness (page 15)
+r1 = 0; r2 = 0; r3 = 0; r4 = 0; r5 = 0; r6 = 0; r7 = 0; r8 = 0
+g1 = 0; g2 = 0; g3 = 0; g4 = 0; g5 = 0; g6 = 0; g7 = 0; g8 = 0
+pattern = [g1, r1, g2, r2, g3, r3, g4, r4,
+           g5, r5, g6, r6, g7, r7, g8, r8
+]
+reverse = [128,64,32,16,8,4,2,1]
+
+#Set gpios to push button
+pushButton1 = "P8_7"
+pushButton2 = "P8_8"
+pushButton3 = "P8_9"
+pushButton4 = "P8_10"
+pushButton5 = "P8_11"
+
+#Parameters used to etch-a-sketch
+width  = 8
+length = 8
+curX = 1
+curY = 1
+clearFlag = 0
+quitFLag = 0
+global value 
+temp = 999; temp2 = 999
+tempReverse = [9,9,9,9,9,9,9,9]; tempReverse2 = [9,9,9,9,9,9,9,9];
+addFlag = 0;
+foo = " "
+redLabel = [[foo for i in range(width)] for j in range(length)]
+
+#Define the input for all gpio pins
+GPIO.setup(pushButton1, GPIO.IN)
+GPIO.setup(pushButton2, GPIO.IN)
+GPIO.setup(pushButton3, GPIO.IN)
+GPIO.setup(pushButton4, GPIO.IN)   
+GPIO.setup(pushButton5, GPIO.IN) 
+
+
+foo = " "
+grid = [[foo for i in range(width+1)] for j in range(length+1)]
+for i in range (width+1):
+    for j in range (length+1):
+        grid[i][j] = " "
+        if (i == 0) & (j > 0):
+            grid[i][j] =str(j-1)
+            # print(grid[i][j])
+        if (j == 0) & (i > 0):
+            grid[i][j] = str(i-1)+":"
+        
+            
+#Print out the grid
+def printGrid():
+    for i in range (width+1):
+        result = ""
+        if i == 0:
+            result += " "
+        for j in range(length+1):
+            result = result + grid[i][j] + "  "
+        
+        print(result)
+
+grid[curX][curY] = "+"
+grid2 = grid
+printGrid()
+pattern[0] = 128
+bus.write_i2c_block_data(matrix, 0, pattern)
+
+reverseRed = [0,0,0,0,0,0,0,0]
+# pdb.set_trace()
+print("Instruction: w to move up; s to move down; a to move left; d to move right; c to clear")
+#Draw the grid according to button pressed
+def drawGrid():
+    # value = callback1(a) #input("Direction=\n")
+    global curX; global curY; global clearFlag; global quitFlag
+    global grid2; global value
+    global pattern; global reverse; 
+    global clearGreen
+    global clearRed
+    global reverseRed
+    global temp; global temp2
+    global tempReverse; global tempReverse2
+    global addFlag
+    global redLabel
+    if value == "w":
+        addFlag = 0;
+        grid[curX][curY] = "*"
+        if redLabel[curX-1][curY-1] != 1:
+            reverseRed[curY-1] += reverse[curX-1]
+        if (reverseRed[curY-1] > 255): reverseRed[curY-1] = 255
+        pattern[curY*2-1] = reverseRed[curY-1]
+        redLabel[curX-1][curY-1] = 1
+        if clearFlag == 1:
+            grid[curX][curY] = " "
+            clearFlag = 0
+        if curX == 1:
+            curX = width
+        else:
+            curX -= 1
+        clearGreen()
+        pattern[curY*2-2] = reverse[curX-1]
+        
+    elif value == "s":
+        addFlag = 0;
+        grid[curX][curY] = "*"
+        if redLabel[curX-1][curY-1] != 1:
+            reverseRed[curY-1] += reverse[curX-1]
+        if (reverseRed[curY-1] > 255): reverseRed[curY-1] = 255
+        pattern[curY*2-1] = reverseRed[curY-1]
+        redLabel[curX-1][curY-1] = 1
+        if clearFlag == 1:
+            grid[curX][curY] = " "
+            clearFlag = 0
+        if curX == width:
+            curX = 1
+        else:
+            curX += 1
+        clearGreen()
+        pattern[curY*2-2] = reverse[curX-1]
+    elif value == "a":
+        grid[curX][curY] = "*"
+        # print("temp" + str(temp) + "  tempReverse" + str(tempReverse))
+        # if (temp == curX) and (tempReverse[curY-1] == reverseRed[curY-1]):
+        #     reverseRed[curY-1] = reverseRed[curY-1]
+        # else:
+        #     reverseRed[curY-1] += reverse[curX-1]
+        # if (reverseRed[curY-1]):
+        #     reverseRed[curY-1] += reverse[curX-1]
+        #     addFlag = 1;
+        if redLabel[curX-1][curY-1] != 1:
+            reverseRed[curY-1] += reverse[curX-1]
+        if (reverseRed[curY-1] > 255): reverseRed[curY-1] = 255
+        print(reverseRed)
+        pattern[curY*2-1] = reverseRed[curY-1]
+        redLabel[curX-1][curY-1] = 1
+        temp = curX; tempReverse[curY-1] = reverseRed[curY-1]
+        if clearFlag == 1:
+            grid[curX][curY] = " "
+            clearFlag = 0
+        if curY == 1:
+            curY = length
+        else:
+            curY -= 1
+        clearGreen()
+        pattern[curY*2-2] = reverse[curX-1]
+        
+    elif value == "d":
+        grid[curX][curY] = "*"
+        if redLabel[curX-1][curY-1] != 1:
+            reverseRed[curY-1] += reverse[curX-1]
+        if (reverseRed[curY-1] > 255): reverseRed[curY-1] = 255
+        pattern[curY*2-1] = reverseRed[curY-1]
+        redLabel[curX-1][curY-1] = 1
+        if clearFlag == 1:
+            grid[curX][curY] = " "
+            clearFlag = 0
+        if curY == length:
+            curY = 1
+        else:
+            curY += 1
+        clearGreen()
+        pattern[curY*2-2] = reverse[curX-1]
+    elif value == "p":
+        for i in range (width+1):
+            for j in range (length+1):
+                grid[i][j] = " "
+                if (i == 0) & (j > 0):
+                    grid[i][j] =str(j-1)
+                    # print(grid[i][j])
+                if (j == 0) & (i > 0):
+                    grid[i][j] = str(i-1)+":"
+        clearGreen()
+        clearRed()
+        reverseRed = [0,0,0,0,0,0,0,0]
+        fo = " "; redLabel = [[fo for i in range(width+1)] for j in range(length+1)]
+        pattern[curY*2-2] = reverse[curX-1]
+    elif value == "q":
+        quitFlag = 1
+    else:
+        print("Say what? I might have heard")
+
+    grid[curX][curY] = "+"
+    if value == "c":
+        clearFlag = 1
+    bus.write_i2c_block_data(matrix, 0, pattern)
+    printGrid()
+
+#Define the callback function for each button pressed
+def callback1(a):
+    global value
+    if(a==pushButton1):
+        value = "w"
+        drawGrid()
+    if(a==pushButton2):
+        value = "a"
+        drawGrid()
+    if(a==pushButton3):
+        value = "s"
+        drawGrid()
+    if(a==pushButton4):
+        value = "d"
+        drawGrid()
+    if(a==pushButton5):
+        value = "p"
+        drawGrid()
+        
+def clearGreen():
+    global pattern
+    for i in range (8):
+        pattern[i * 2] = 0
+def clearRed():
+    global pattern
+    for i in range (8):
+        pattern[i * 2+1] = 0
+
+#GPIO.remove_event_detect(BT1)
+GPIO.add_event_detect(pushButton1, GPIO.RISING, callback=callback1) 
+GPIO.add_event_detect(pushButton2, GPIO.RISING, callback=callback1) 
+GPIO.add_event_detect(pushButton3, GPIO.RISING, callback=callback1) 
+GPIO.add_event_detect(pushButton4, GPIO.RISING, callback=callback1) 
+GPIO.add_event_detect(pushButton5, GPIO.RISING, callback=callback1) 
+
+# bus.write_i2c_block_data(matrix, 0, pattern)
+
+while (1):
+    global quitFlag
+    # drawGrid()
+ 
+
+
+
+
+
