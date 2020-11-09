a6225d3534d40cbc4a6b0f948bca89b3f2a8abb7
Demo User <debian@beaglebone.localdomain>
2020-11-09 08:08:17 +0000

    update

--- Final/test2.py
+++ Final/test2.py
@@ -1,10 +1,41 @@
 import numpy as np
 import cv2
+import pdb
+import time
 
-cap = cv2.VideoCapture(-1)
-
+cap = cv2.VideoCapture(0)
+if cap.isOpened():
+    print ("camera is working")
+else:
+    print ("camera not working")
+    
+cap.open(0)
+time.sleep(1)
+cap.set(3,600)
+cap.set(4,500)
+cap.set(cv2.CAP_PROP_FPS, 1)
 ret, frame = cap.read()
-cv2.imshow('frame',frame)
+
+# frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+lower_blue = np.array([0,50,50])
+upper_blue = np.array([50,255,255])
+
+    # Threshold the HSV image to get only blue colors
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # Bitwise-AND mask and original image
+res = cv2.bitwise_and(frame,frame, mask= mask)
+cv2.imwrite("frame.png", res)
+
+# path = '/~/image.jpg'
+# img = cv2.imread(path)
+# print(img)
+
+# cv2.imwrite("frame.png", frame)
 # while(True):
 
 #     ret, frame = cap.read()
